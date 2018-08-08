4052f505bcb7299f6051ca38f6619e743deb5496
olkruglova <olkruhlova@gmail.com>
2018-08-08 13:52:46 +0200

    Add option: 'books can change a shelf'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Components } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
@@ -6,24 +7,34 @@ import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     showSearchPage: false,
-    books: []
+    Books: []
   }
+    componentDidMount(){
+      this.fetch_books_details()
+    }
+     fetch_books_details = () => {
+      BooksAPI.getAll().then((books)=> {
+        this.setState({
+          Books: books
+        })
+      })
+    }
+     update_books_details = (book, shelf) =>{
+      console.log(book);
+      console.log(shelf);
+      BooksAPI.update(book, shelf).then(()=>{
+        this.fetch_books_details()
+      })
+    }
+
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
-          <BookList />
-        )}
+        <BookList
+          books={this.state.Books}
+          onChange={this.update_books_details}/>
       </div>
     )
   }
--- src/BookList.js
+++ src/BookList.js
@@ -3,6 +3,9 @@ import Shelf from './Shelf'
 
 class BookList extends Component {
   render () {
+
+    const books = this.props.books
+
     return(
       <div className="list-books">
         <div className="list-books-title">
@@ -10,7 +13,23 @@ class BookList extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <Shelf/>
+            <Shelf
+                books={books.filter((book)=>(
+                    book.shelf === "currentlyReading"
+                ))}
+                title = "Currently Reading"
+                onChangeShelf={this.props.onChange}/>
+                <Shelf books={books.filter((book)=>(
+                    book.shelf === "read"
+                ))}
+                title = "Read"
+                onChangeShelf={this.props.onChange}/>
+                <Shelf books={books.filter((book)=>(
+                    book.shelf === "wantToRead"
+                ))}
+                title = "Want to Read"
+                onChangeShelf={this.props.onChange}
+            />
           </div>
         </div>
         <div className="open-search">
