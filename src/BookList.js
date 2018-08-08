26ab2f05b9c95f0c59592e9fa88edc04eb40be90
olkruglova <olkruhlova@gmail.com>
2018-08-08 13:06:35 +0200

    Create Components: Book, Shelf, BookList, Search

---
+++ src/BookList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Shelf from './Shelf'
+
+class BookList extends Component {
+  render () {
+    return(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf/>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+ export default BookList;
