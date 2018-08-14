9aebff52a977918761a712996598b7c7256728d4
olkruglova <olkruhlova@gmail.com>
2018-08-14 10:24:44 +0200

    Fix: separate book autors by commas

--- src/Books.js
+++ src/Books.js
@@ -33,7 +33,7 @@ change_bookShelf = (e) => {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
         </div>
       </li>
     )
